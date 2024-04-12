5cd00b00739a166616ba78c2eaafd9ba3641b1d0
Suraj Gyaneshwar Waghmare <waghmaresuraj159@gmail.com>
2024-04-13 01:13:27 +0530

    .

--- vercelApp/src/App.js
+++ vercelApp/src/App.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes,Navigate } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
 import AdminDashboard from './pages/AdminDashboard';
 import VoterDashboard from './pages/VoterDashboard';
 import AdminDemo from './pages/AdminDemo';
+import VoterDemo from './pages/VoterDemo';
 
 function App() {
-  const [user, setUser] = useState(null);
 
   return (
     
       <Router>
         <Routes>
           <Route path="/" element={<AdminDemo/>}/>
+          <Route path="/votedemo" element={<VoterDemo/>}/>
           <Route path="/admin" element={ <AdminDashboard /> } />
           <Route path="/voter" element={ <VoterDashboard /> } />
         </Routes>
