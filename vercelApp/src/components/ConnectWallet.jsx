ba4bfb542d854fed49d31fa956cde60440cf09ec
Suraj Gyaneshwar Waghmare <waghmaresuraj159@gmail.com>
2024-05-05 19:55:00 +0530

    update

--- vercelApp/src/components/ConnectWallet.jsx
+++ vercelApp/src/components/ConnectWallet.jsx
@@ -19,7 +19,7 @@ async function ConnectWallet() {
 				chainName: `Sepolia test network`,
 				chainId: "0xaa36a7",
 				nativeCurrency: { name: "ETH", symbol: "eth", decimals: 18 },
-				rpcUrls: [`wss://ethereum-sepolia-rpc.publicnode.com`],
+				rpcUrls: [`https://sepolia.drpc.org`],
 				blockExplorerUrls: [`https://sepolia.etherscan.io/`],
 			},
 		],
